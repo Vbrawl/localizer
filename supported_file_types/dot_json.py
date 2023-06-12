3ee8d76028a02a2be4a7d292dd2830eaf4e247ef
Vbrawl <greninjagr@gmail.com>
2023-06-12 16:53:05 +0300

    Added ability to export and parse sets.

--- supported_file_types/dot_json.py
+++ supported_file_types/dot_json.py
@@ -3,11 +3,18 @@ from localizer.FileHandler import FileHandler
 
 class FHandler(FileHandler):
     @staticmethod
-    def parse(file:str) -> dict[str, str]:
+    def parse(file:str) -> dict[str, str] | set[str]:
         with open(file, 'r') as f:
-            return json.load(f)
+            out = json.load(f)
+            if isinstance(out, list):
+                return set(out)
+            else:
+                return out
 
     @staticmethod
     def export(file:str, texts:dict[str, str] | set[str]) -> None:
         with open(file, 'w') as f:
-            json.dump(texts, f)
+            if isinstance(texts, set):
+                json.dump(list(texts), f)
+            else:
+                json.dump(texts, f)
