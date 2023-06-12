f2fa826972b283db47641a3f48bb6cc669b060c5
Vbrawl <greninjagr@gmail.com>
2023-06-12 14:57:47 +0300

    Added a LanguagePack object for the custom print.

--- print_replace.py
+++ print_replace.py
@@ -1,6 +1,9 @@
 from io import TextIOWrapper
 import sys
 from typing import Optional, TextIO
+from localizer.language_pack import LanguagePack
+
+PRINT_LANGUAGE_PACK = LanguagePack() # An empty language pack to avoid errors.
 
 def print(*values: object, sep:Optional[str] = ' ', end:Optional[str] = '\n', file:Optional[TextIOWrapper | TextIO] = None, flush:bool = False) -> None:
     generate_error_message = lambda name, value: f"{name} must be None or a string, not {' '.join(str(type(value)).split(' ')[1:])[1:-2]}"
@@ -12,7 +15,7 @@ def print(*values: object, sep:Optional[str] = ' ', end:Optional[str] = '\n', fi
     if file is None:
         file = sys.stdout
 
-    rendered_text = (sep or '').join(  map(lambda x: str(x), values)  ) + str(end or '')
+    rendered_text = (sep or '').join(  map(lambda x: PRINT_LANGUAGE_PACK.gettext(x) if isinstance(x, str) else str(x), values)  ) + str(end or '')
     lrt = len(rendered_text)    # length rendered text
     wtl = 0                     # written text length
 
