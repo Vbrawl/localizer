b37371734dd633ed6beb43800f971b201545b42d
Vbrawl <greninjagr@gmail.com>
2023-06-13 01:40:21 +0300

    Fixed a broken import.

--- supported_file_types/__init__.py
+++ supported_file_types/__init__.py
@@ -1,5 +1,5 @@
 from localizer.supported_file_types import dot_csv, dot_json
-from FileHandler import FileHandler
+from localizer.FileHandler import FileHandler
 
 FILE_TYPES:dict[str, type[FileHandler]] = {
     ".csv": dot_csv.FHandler,
