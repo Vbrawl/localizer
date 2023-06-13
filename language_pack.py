1b4eb9266349c80bb967eef1ef0cc407a219b52b
Vbrawl <greninjagr@gmail.com>
2023-06-13 14:52:03 +0300

    Added docstrings to later use sphinx autodocs.

--- language_pack.py
+++ language_pack.py
@@ -12,11 +12,26 @@ class LanguagePack:
             self.set_file_extension(ext, fhandler)
 
     def set_file_extension(self, extension:str, fhandler:type[FileHandler]):
+        """Creates a connection between the specified `extension` and the specified `FileHandler`
+        which allows to later process files of that extension with the correct `FileHandler`.
+
+        Args:
+            extension (str): The extension of the file that the Handler can process (Example, .json, .csv).
+            fhandler (type[FileHandler]): A reference to the class holding the static methods for parsing and exporting the files.
+        """
+
         if not extension.startswith('.'):
             extension = '.' + extension
         self.supported_file_types[extension] = fhandler
 
     def export_file(self, file:str) -> None:
+        """Exports the data of the language pack to the specified `file`.
+        The data is processed and stored using the `FileHandler` for the specific file format.
+
+        Args:
+            file (str): A string with the filepath/filename of the file. (The file will be created if it doesn't exist).
+        """
+
         for extension, fhandler in self.supported_file_types.items():
             if file.endswith(extension):
                 new_texts_file = file[:-len(extension)]+'.new_text'+extension
@@ -25,10 +40,21 @@ class LanguagePack:
                     fhandler.export(new_texts_file, self.new_texts)
 
     def parse_file(self, file:str) -> None:
+        """Parses the data of `file` and stores them in the language pack.
+        The data is processed and stored using the `FileHandler` for the specific file format.
+
+        Args:
+            file (str): A string with the filepath/filename of the file.
+
+        Raises:
+            TypeError: If there is no handler available to process this file.
+        """
         o_t:Optional[dict[str, str]] = None
         for extension, fhandler in self.supported_file_types.items():
             if file.endswith(extension):
-                o_t = fhandler.parse(file)
+                x = fhandler.parse(file)
+                if isinstance(x, dict):
+                    o_t = x
         if o_t is None:
             raise TypeError("File must end with one of the following extensions: " + ', '.join(self.supported_file_types.keys()))
 
@@ -40,9 +66,24 @@ class LanguagePack:
                 pass # o doesn't exist to begin with.
     
     def add_translation(self, original:str, translated:str) -> None:
+        """Add a translation for a sentence in the language pack.
+
+        Args:
+            original (str): The original sentence.
+            translated (str): The translated sentence.
+        """
         self.o_t[original] = translated
     
     def gettext(self, text:str) -> str:
+        """Get the translation of `text` if it exists,
+        otherwise fallback to the original translation.
+
+        Args:
+            text (str): The text to be translated.
+
+        Returns:
+            str: The translated text, or the original text (If no translation was found).
+        """
         if text == '' or text in self.new_texts:
             return text
         elif text in self.o_t.keys():
--- print_replace.py
+++ print_replace.py
@@ -5,6 +5,21 @@ from localizer.language_pack import LanguagePack
 PRINT_LANGUAGE_PACK = LanguagePack() # An empty language pack to avoid errors.
 
 def print(*values: object, sep:Optional[str] = ' ', end:Optional[str] = '\n', file:Optional[IO] = None, flush:bool = False) -> None:
+    """An emulated version of the default `print`.
+    Allows `localizer` module to intercept the values passed to `print`
+    and provide an easy way to translate them to different languages.
+
+    Args:
+        sep (Optional[str], optional): The separator, a value that's placed between the values. Defaults to ' '.
+        end (Optional[str], optional): The ending value, a value that's placed at the end of the whole value list. Defaults to '\n'.
+        file (Optional[IO], optional): The file object in which to write the buffer, None is replaced with STDOUT. Defaults to None.
+        flush (bool, optional): Wether to flush the buffer or not. Defaults to False.
+
+    Raises:
+        TypeError: If `sep` is not of type `str` and not `None`.
+        TypeError: If `end` is not of type `str` and not None.
+    """
+
     # Shortcut for generating error messages.
     generate_error_message = lambda name, value: f"{name} must be None or a string, not {' '.join(str(type(value)).split(' ')[1:])[1:-2]}"
 
