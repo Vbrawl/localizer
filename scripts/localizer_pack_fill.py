c6c698313383741483137c14f36a2d21729efb90
Vbrawl <greninjagr@gmail.com>
2023-06-14 02:35:33 +0300

    Added recommendations and auto translation.

--- scripts/localizer_pack_fill.py
+++ scripts/localizer_pack_fill.py
@@ -1,25 +1,51 @@
-import argparse, os
+import argparse, os, translators as ts
 from localizer.language_pack import LanguagePack
+
+def translate_text(query_text:str, from_language:str="auto", to_language:str="en", translators:list[str]=["deepl", "google", "bing"], tries=5) -> str:
+    for translator in translators:
+        for i in range(tries):
+            try:
+                return ts.translate_text(query_text=query_text, translator=translator, from_language = from_language, to_language = to_language) # type: ignore
+            except Exception:
+                pass
+    else:
+        return ""
+
+
+def get_translation(original:str, recommendation:str, prompt:str, qtranslator:bool = False):
+    if qtranslator:
+        return recommendation
+
+    translated = input(prompt.format(original = original, recommendation = (" (" + recommendation + ")" if recommendation else "")))
+    if recommendation and not translated:
+        translated = recommendation
+    return translated
 
 parser = argparse.ArgumentParser(description="Create a language pack for the Python package `Localizer`.", add_help=True, exit_on_error=False)
 parser.add_argument("language_pack_file", type=str, help="The language_pack file, this is going to be updated if it doesn't exist.")
 parser.add_argument("--external_file", "-ef", type=str, nargs="*", help="Other language_packs(to get the original texts only), or new_texts files.")
-#parser.add_argument("--translator", "-t", action="store_true", help="Use google translate to provide recommendations.")
-#parser.add_argument("--quick-translator", "-qt", action="store_true", help="Use google translate to automatically translate everything. (Automated translation!)")
+parser.add_argument("--translator", "-t", action="store_true", help="Use a translator API to provide translation recommendations.")
+parser.add_argument("--quick-translator", "-qt", action="store_true", help="Use a translator API to automatically translate everything. (Automated translation!)")
+parser.add_argument("--from-lang", "-fl", type=str, default="auto", help="The language of the original text (language code).")
+parser.add_argument("--to-lang", "-tl", type=str, help="The language of the translated text (language code).")
 
 args = parser.parse_args()
 
 lpf = args.language_pack_file
 efs = args.external_file
-#translator = args.translator
-#qtranslator = args.quick_translator
-translator = False      # NOT IMPLEMENTED
-qtranslator = False     # NOT IMPLEMENTED
+translator = args.translator
+qtranslator = args.quick_translator
+fl = args.from_lang
+tl = args.to_lang
 
 lp = LanguagePack()
 
 if translator and qtranslator:
     print("Error: Cannot enable translator and quick-translator at the same time.")
+    exit()
+
+if (translator or qtranslator) and not tl:
+    print("Error: --to-lang needs to be specified.")
     exit()
 
 if os.path.exists(lpf):
@@ -36,13 +62,15 @@ if efs:
             for nt in tmp_lp.new_texts:
                 lp.new_texts.add(nt)
         except TypeError or PermissionError:
-            print("Error: external_file({f}) doesn't exist. Ignoring...")
+            print(f"Error: external_file({f}) doesn't exist. Ignoring...")
 
 if lp.new_texts:
     print("First, please translate all the new_texts that were found:")
     for nt in lp.new_texts:
-        translated = input(f"{nt} => ")
-        lp.add_translation(nt, translated)
+        recommendation = translate_text(nt, from_language=fl, to_language=tl) if (translator or qtranslator) else ""
+        translated = get_translation(nt, recommendation, "{original}{recommendation} => ", qtranslator)
+        if translated:
+            lp.add_translation(nt, translated)
 
 yes_NO = input("Would you like to add more texts? [yes/NO]: ")
 print("When translation is empty, the original text enters the new_texts.")
@@ -53,7 +81,8 @@ if yes_NO and yes_NO.upper() == "YES":
         original = input("Original Text: ")
 
         if original:
-            translated = input("Translated Text: ")
+            recommendation = translate_text(original, from_language=fl, to_language=tl) if (translator or qtranslator) else ""
+            translated = get_translation(original, recommendation, "Translated Text{recommendation}:", qtranslator)
 
             if translated:
                 lp.add_translation(original, translated)
