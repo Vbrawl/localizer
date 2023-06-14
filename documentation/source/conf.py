f8078a6ac1ca97fba11ac0f2eeadf482fa250e44
Vbrawl <greninjagr@gmail.com>
2023-06-14 17:24:33 +0300

    Updated documentation and version.

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -9,7 +9,7 @@
 project = 'Localizer'
 copyright = '2023, Vbrawl'
 author = 'Vbrawl'
-release = '1.1'
+release = '1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
